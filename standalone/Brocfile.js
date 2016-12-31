f458d6df2e708e3d34340f0f75aaf2cc2fd20ebd
Pablo Brasero <pablo@pablobm.com>
2016-12-31 19:18:42 +0000

    Concatenate several ES6 modules together

--- standalone/Brocfile.js
+++ standalone/Brocfile.js
@@ -1,6 +1,22 @@
 var esTranspiler = require('broccoli-babel-transpiler');
+var concat = require('broccoli-concat');
+var MergeTrees = require('broccoli-merge-trees');
 
+var vendorTree = 'external';
 var srcTree = 'src';
-var es6Tree = esTranspiler(srcTree);
 
-module.exports = es6Tree;
+var transpiledSrcTree = esTranspiler(srcTree, {
+  moduleIds: true,
+  modules: 'amd',
+});
+
+var jsTree = new MergeTrees([vendorTree, transpiledSrcTree]);
+
+var appTree = concat(jsTree, {
+  headerFiles: ['vendor/loader.js'],
+  footerFiles: ['auto-start.js'],
+  inputFiles: '**/*.js',
+  outputFile: 'index.js',
+});
+
+module.exports = appTree;
