5e4932a03dc8118012c08673ab809d69fc313237
Pablo Brasero <pablo@pablobm.com>
2017-01-15 12:39:11 +0000

    Simplify

--- standalone/Brocfile.js
+++ standalone/Brocfile.js
@@ -1,28 +1,27 @@
 var babelTranspiler = require('broccoli-babel-transpiler');
-var Funnel          = require('broccoli-funnel');
 var MergeTrees      = require('broccoli-merge-trees');
-var sass            = require('broccoli-sass-source-maps');
+var sass            = require('broccoli-sass');
 var source          = require('broccoli-source');
 var watchify        = require('broccoli-watchify');
 
 var WatchedDir = source.WatchedDir;
 
-var root = new WatchedDir('.');
+var inputHtmlTree = new WatchedDir('src/html');
 var inputJsTree = new WatchedDir('src/js');
 var inputScssTree = new WatchedDir('src/styles');
+
+var outputHtmlTree = inputHtmlTree;
 
 var transpiledTree = babelTranspiler(inputJsTree);
 var outputJsTree = watchify(transpiledTree, {
   browserify: {
     entries: ['app.js'],
-    paths: [__dirname + '/node_modules'],
+    paths: ['.', __dirname + '/node_modules'],
   },
   outputFile: 'index.js',
 });
 
 var outputCssTree = sass([inputScssTree], 'app.scss', 'index.css', {});
-
-var outputHtmlTree = new Funnel(root, { srcDir: 'src', destDir: '.', include: ['index.html'] });
 
 module.exports = new MergeTrees([
   outputCssTree,
