00b8a7b9d94ddc3dab9796dcebc3d15a123e4982
Pablo Brasero <pablo@pablobm.com>
2017-01-16 20:38:20 +0000

    Add the Sass plugin and merge the trees

--- blog-post/Brocfile.js
+++ blog-post/Brocfile.js
@@ -1,6 +1,16 @@
-var source = require('broccoli-source');
+var MergeTrees = require('broccoli-merge-trees');
+var sass       = require('broccoli-sass');
+var source     = require('broccoli-source');
+
 var WatchedDir = source.WatchedDir;
+
 var inputHtml = new WatchedDir('src/html');
 var outputHtml = inputHtml;
 
-module.exports = outputHtml;
+var inputStyles = new WatchedDir('src/styles');
+var outputCss = sass([inputStyles], 'app.scss', 'index.css');
+
+module.exports = new MergeTrees([
+  outputCss,
+  outputHtml,
+]);
