539b656d39a5445402c21c52310a516950e0081b
Pablo Brasero <pablo@pablobm.com>
2017-01-16 20:09:45 +0000

    For consistency later

--- blog-post/Brocfile.js
+++ blog-post/Brocfile.js
@@ -1,5 +1,6 @@
 var source = require('broccoli-source');
 var WatchedDir = source.WatchedDir;
 var inputHtml = new WatchedDir('src/html');
+var outputHtml = inputHtml;
 
-module.exports = inputHtml;
+module.exports = outputHtml;
