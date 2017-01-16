93ca6621101f194457ce4d8b742222537121c57c
Pablo Brasero <pablo@pablobm.com>
2017-01-16 21:07:27 +0000

    From ES6 to ES5, with Babel

--- blog-post/Brocfile.js
+++ blog-post/Brocfile.js
@@ -1,11 +1,15 @@
-var MergeTrees = require('broccoli-merge-trees');
-var sass       = require('broccoli-sass');
-var source     = require('broccoli-source');
+var babelTranspiler = require('broccoli-babel-transpiler');
+var MergeTrees      = require('broccoli-merge-trees');
+var sass            = require('broccoli-sass');
+var source          = require('broccoli-source');
 
 var WatchedDir = source.WatchedDir;
 
 var inputHtml = new WatchedDir('src/html');
 var outputHtml = inputHtml;
+
+var inputJs = new WatchedDir('src/js');
+var outputJs = babelTranspiler(inputJs);
 
 var inputStyles = new WatchedDir('src/styles');
 var outputCss = sass([inputStyles], 'app.scss', 'index.css');
@@ -13,4 +17,5 @@ var outputCss = sass([inputStyles], 'app.scss', 'index.css');
 module.exports = new MergeTrees([
   outputCss,
   outputHtml,
+  outputJs,
 ]);
---
+++ blog-post/src/js/index.js
@@ -0,0 +1,6 @@
+const print = (...args) => {
+  console.log(...args);
+};
+
+print("output from app.js, in ES6");
+
