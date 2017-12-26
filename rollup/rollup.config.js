2a05737b35670224c2359f96c5169db1f8b0966d
Pablo Brasero <pablo@thoughtbot.com>
2017-12-26 13:14:09 +0000

    Use plugins to import modules from `node_modules`

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,7 +1,14 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
   input: 'js/app.js',
   output: {
     file: 'dist/index.js',
     format: 'umd'
-  }
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve()
+  ]
 };
